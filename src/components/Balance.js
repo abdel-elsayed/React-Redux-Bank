9b2bab79c385a93242c8234b53129b62c561d8bc
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-01-16 16:32:57 -0500

    initial commit

---
+++ src/components/Balance.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class Balance extends Component {
+  
+
+    render() {
+        return (
+            <div>
+                <h1>Balance: $ 0</h1>
+            </div>
+        )
+    }
+}
+
+export default Balance
---
+++ src/components/Debit.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+ class Debit extends Component {
+    render() {
+        return (
+            <div>
+                <form >
+                <label>Debit Amount: </label><br />
+                <input type="text" name="debitAmount" placeholder="Enter Amount..." ></input>
+                <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+
+export default (Debit)
